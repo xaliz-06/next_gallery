c87e4746af5db0be862c7d34779208992d2d817c
xaliz-06 <vaibhavchakraborty06@gmail.com>
2025-01-22 14:02:56 +0530

    added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,28 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function Navbar() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-2">
+      <div className="px-4 py-2">
+        <h1 className="text-3xl font-semibold">My Food Gallery</h1>
+      </div>
+      <div className="px-4 py-2">
+        <h1 className="text-3xl font-semibold">Sign in</h1>
+      </div>
+    </nav>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className="flex flex-col gap-4">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
