1d004f8a197860a60fa0ab799966a0056336224f
xaliz-06 <vaibhavchakraborty06@gmail.com>
2025-01-22 23:51:50 +0530

    added authentication

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,28 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default function Navbar() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-2">
+      <div className="px-4 py-2">
+        <h1 className="text-3xl font-semibold">My Food Gallery</h1>
+      </div>
+      <div className="px-4 py-2">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <div className="flex items-center justify-between gap-3">
+            <UserButton />
+            <SignOutButton />
+          </div>
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,36 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const imagesLinks = [
-  "https://eoa1bgd6ac.ufs.sh/f/qqPmVpKVG1LOjR2WeJzy9pvFWxCtVcKdNreBT3kOE5ZfXIgm",
-  "https://eoa1bgd6ac.ufs.sh/f/qqPmVpKVG1LOEn9eTwUKL0ThwxPmdHR43CFIseAkvj8iQ57V",
-  "https://eoa1bgd6ac.ufs.sh/f/qqPmVpKVG1LOWUIIFWpL4gxYv19jJXZKc65huiyHfMeqCT7P",
-  "https://eoa1bgd6ac.ufs.sh/f/qqPmVpKVG1LOLqhUtCn8PMCuoyeJqF9N6rKfzdHXi3vSURt1",
-];
-
-const mockImages = [...imagesLinks, ...imagesLinks, ...imagesLinks].map(
-  (link, index) => ({
-    id: index,
-    src: link,
-    alt: `Image ${index + 1}`,
-  }),
-);
-
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
   });
 
   return (
+    <div className="flex flex-wrap">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div
+          key={index}
+          className="m-2 flex w-1/5 items-center justify-center bg-slate-400 p-2"
+        >
+          <img src={image.url} alt={image.name} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default function HomePage() {
+  return (
     <main className="flex min-h-screen flex-col items-center justify-center px-6 py-12">
-      <div className="flex flex-wrap">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div
-            key={index}
-            className="m-2 flex w-1/5 items-center justify-center bg-slate-400 p-2"
-          >
-            <img src={image.url} alt={image.name} />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <h2 className="h-full w-full text-center text-3xl">Please Sign In</h2>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
